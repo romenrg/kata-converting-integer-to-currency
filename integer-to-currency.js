236212178cd41a8b6ffa3b52921c2afe8bc5ffc0
Romén Rodríguez <romenrg@gmail.com>
2015-12-11 15:26:46 +0000

    Solved using an arithmetic approach

--- integer-to-currency.js
+++ integer-to-currency.js
@@ -1,5 +1,35 @@
-function toCurrency(price) {
-    return price.toString();
+function toCurrency(price){
+    var stringResult = "";
+    while (price >= 1000) {
+        stringResult = _prependCommaIfNeeded(stringResult);
+        stringResult = _concatenateNewModulus(price, stringResult);
+        price = parseInt(price/1000);
+    }
+    var remainder = price % 1000;
+    stringResult = _prependCommaIfNeeded(stringResult);
+    stringResult = (remainder).toString().concat(stringResult);
+    return stringResult;
+}
+
+function _concatenateNewModulus(price, stringResult) {
+    var tempRightPart = price % 1000;
+    if (tempRightPart >= 100) {
+        stringResult = (tempRightPart).toString().concat(stringResult);
+    }
+    else if (tempRightPart >= 10) {
+        stringResult = "0".concat(tempRightPart + stringResult);
+    }
+    else {
+        stringResult = "00".concat(tempRightPart + stringResult);
+    }
+    return stringResult;
+}
+
+function _prependCommaIfNeeded(stringResult) {
+    if(stringResult.match(/^\d/)) {
+        stringResult = ",".concat(stringResult);
+    }
+    return stringResult;
 }
 
 module.exports = toCurrency;
